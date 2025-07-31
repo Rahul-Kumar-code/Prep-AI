118e65ff12410294182b0a0b7a25d9b6bd1779c1
Rahul-Kumar-code <rk0505469@gmail.com>
2025-07-31 12:45:37 +0530

    fixed

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const {generateInterviewQuestions, generateConceptExplanation}  = require('./con
 const app = express();
 
 app.use(cors({
-  origin: ["https://prep-ai-one-lovat.vercel.app/"],
+  origin: ["https://prep-ai-one-lovat.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
